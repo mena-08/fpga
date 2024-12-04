6256022767e1f88831f1b4e2eeb65cd889e1e928
Omar Mena <reach_71@hotmail.com>
2024-12-05 01:32:42 +0300

    Updated correct graphics_driver.sv

--- source_files/graphics_driver.sv
+++ source_files/graphics_driver.sv
@@ -22,5 +22,61 @@
 
 module graphics_driver(
 
+    input logic pixel_clk, rst, blank_in, hs_in, vs_in,
+    input logic [10:0]drawX, drawY,
+
+    input [11:0]background, foreground,
+    
+    output logic hs, vs, frame_clk,
+    output [11:0]RGB
+
     );
+
+    //Delay VGA signals by 2 clock cycles
+    logic hs_reg0, vs_reg0, blank_reg0, blank;
+
+    always_ff @ (posedge pixel_clk) begin
+    
+        
+        if (rst) begin
+            hs <= 10'd0;
+            vs <= 10'd0;
+            blank = 0; 
+        end 
+    
+        blank_reg0 <= blank_in;
+        hs_reg0 <= hs_in;
+        vs_reg0 <= vs_in;        
+        
+        blank <= blank_reg0;
+        hs <= hs_reg0;
+        vs <= vs_reg0;
+    
+    end   
+    
+    
+    logic [11:0]color;
+    parameter transparent_mask = 12'h000;
+    assign RGB = color;
+    
+    //  Set the transparency of color    
+    always_comb begin
+    
+        if(blank)
+            color = 12'd0;
+        else begin
+            if(foreground == transparent_mask)
+                color = background;
+            else
+                color = foreground;
+        end
+        
+    end
+    
+//blk_mem_gen_0 your_instance_name (
+//  .clka(pixel_clk),    // input wire clka
+//  .addra(drawX + drawY*640),  // input wire [10 : 0] addra
+//  .douta(background)  // output wire [11 : 0] douta
+//);
+    
 endmodule
